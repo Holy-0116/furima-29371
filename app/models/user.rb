3bf3d5a9da1901195ff563c5a439f3a8d2221996
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-09-01 16:56:00 +0900

    Userモデルのバリデーション設定

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,12 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-
+  with_options presence: true do
+    validates :nickname
+    validates :name_first, format: { with: /\A[ぁ-んァ-ン一-龥]/, message:"is invalid. Input full-width characters."}
+    validates :name_family, format: { with: /\A[ぁ-んァ-ン一-龥]/, message:"is invalid. Input full-width characters."} 
+    validates :name_first_reading,format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."} 
+    validates :name_family_reading,format: { with: /\A[ァ-ヶー－]+\z/, message: "is invalid. Input full-width katakana characters."} 
+    validates :birthday
+  end
 end
