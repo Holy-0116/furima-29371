144f14e92d86805f2cf16cab7c105e950b492bd4
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-09-01 20:49:59 +0900

    deviseへのパラメーター許可実装

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  before_action :configure_permitted_parameters, if: :devise_controller?
+
+  protected
+
+  def configure_permitted_parameters
+    # binding.pry
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:nickname, :name_first, :name_family, :name_first_reading, :name_family_reading, :birthday])
+  end
 end
