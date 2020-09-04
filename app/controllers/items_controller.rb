8e30d1b5ba4660bb997f52ec89873b4e5a9238f2
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-09-04 17:46:58 +0900

    出品ページへ遷移の制限実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,4 +1,5 @@
 class ItemsController < ApplicationController
+  before_action :get_root, only:[:new]
   def index
     
   end
@@ -6,5 +7,16 @@ class ItemsController < ApplicationController
   def new
     @item = Item.new
   end
-  
+
+
+
+  private
+
+  def get_root
+    unless current_user
+      redirect_to '/users/sign_in'
+    end
+  end
+
 end
+
