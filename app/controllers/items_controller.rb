f6db19b85d844ee0bc391e0c1a242ebce7f6f8a8
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-09-04 23:38:00 +0900

    Itemコントローラー出品機能実装

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -8,7 +8,14 @@ class ItemsController < ApplicationController
     @item = Item.new
   end
 
-
+  def create
+    @item = Item.new(item_params)
+    if @item.save
+      redirect_to '/'
+    else
+      render :new
+    end
+  end
 
   private
 
@@ -16,6 +23,10 @@ class ItemsController < ApplicationController
     unless current_user
       redirect_to '/users/sign_in'
     end
+  end
+
+  def item_params
+    params.require(:item).permit(:image,:name,:explanation,:price,:category_id,:status_id,:delivery_fee_id,:delivery_day_id,:shipping_origin_id).merge(user_id: current_user.id)
   end
 
 end
