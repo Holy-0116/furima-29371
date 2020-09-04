cb39272a6f05b6dd7cd47173ad98ffc132821606
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-09-04 17:03:52 +0900

    カテゴリモデルの修正

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,7 +1,7 @@
 class Category < ActiveHash::Base
   self.data = [
     {id: 1, name:'--'},
-    {id: 2, name:'レディース'}
+    {id: 2, name:'レディース'},
     {id: 3, name:'ベビー・キッズ'},
     {id: 4, name:'インテリア・住まい・小物'},
     {id: 5, name:'本・音楽・ゲーム'},
