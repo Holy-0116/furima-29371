b2abc761f76d81f87e5a0bad3abd5f82c490ee28
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-09-05 16:24:44 +0900

    Itemモデルのテスト用FactoryBot作成

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,5 +1,15 @@
 FactoryBot.define do
   factory :item do
+    association :user
+    name               {Faker::Lorem.sentence}
+    explanation        {Faker::Lorem.sentence}
+    price              {1000}
+    category_id        {"2"}
+    status_id          {"2"}
+    delivery_fee_id    {"2"}
+    delivery_day_id    {"2"}
+    shipping_origin_id {"2"}
+
     
   end
 end
