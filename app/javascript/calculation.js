d06dc3ed9773b7d36e4c132c95a37826739f0991
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-09-05 11:44:02 +0900

    利益計算のJS実装

---
+++ app/javascript/calculation.js
@@ -0,0 +1,12 @@
+function calculationProfit(){
+  const priceForm = document.getElementById("item-price");
+  const tax = document.getElementById("add-tax-price");
+  const profit = document.getElementById("profit");
+
+  priceForm.addEventListener("change",() =>{
+    let price = priceForm.value
+    tax.innerHTML = Math.round(price * 0.1)
+    profit.innerHTML = price - tax.innerHTML  
+  });
+}
+window.addEventListener("load",calculationProfit);
