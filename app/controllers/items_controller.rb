6061d3a93b3e370a89a4e566bc147c543845b209
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-09-07 18:21:31 +0900

    Itemコントローラー追加修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -34,13 +34,11 @@ class ItemsController < ApplicationController
   end
 
   def destroy
-    if @item.user == current_user
       if @item.destroy
         redirect_to root_path
       else
         render :show
       end
-    end
   end
 
   private
