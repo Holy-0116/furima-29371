22e45514ec2c2c7c99b19f38feb01a3768e4e66d
Holy-0116 <69621399+Holy-0116@users.noreply.github.com>
2020-09-08 11:39:36 +0900

    対象商品を入れたインスタンス変数定義

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,3 +1,7 @@
-class OrdersController < ApplicationControlle
+class OrdersController < ApplicationController
+  def index
+    @order = Order.new
+    @item = Item.find_by(id: params[:item_id])
+  end
 
 end
